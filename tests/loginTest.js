bb6fc122cc5d7b1651c30760fa48763c88c8a282
guimora <43528622+guimora@users.noreply.github.com>
2018-09-23 23:17:34 -0500

    Add files via upload

--- tests/loginTest.js
+++ tests/loginTest.js
@@ -2,7 +2,7 @@ const data = require('../test_data/data');
 const page = require('../page_objects/page')
 const loginPage = require('../page_objects/loginPage');
 const userPage = require('../page_objects/userPage');
-
+//
 beforeAll(function () {
   page.openUrl()
 })
@@ -13,6 +13,7 @@ describe("SignIn into the Site", function() {
         loginPage.negativeTest();
         expect(userPage.isErrorMessageDisplayed()).toBe(true);
     });
+    
     //positiveTest
     it("Enter application", function() {
         loginPage.positiveTest(data.email, data.firstName, data.lastName, data.emailForm, data.passwordTextField, data.addressFirstName, data.addressLastName, data.addressCompanyName, data.address, data.addressSecondLine, data.addressCity, data.addressZipCode, data.additionalComments, data.homePhone, data.mobilePhone, data.addressAlias);
